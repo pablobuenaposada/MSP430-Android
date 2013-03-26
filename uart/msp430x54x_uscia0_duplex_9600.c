888bba6520b13a0b0c39cc0aa250ad339889255d
Pablo <pablo.buenaposada@gmail.com>
2013-03-27 00:13:48 +0100

    now msp430 can read full strings and first things with digital output

--- uart/msp430x54x_uscia0_duplex_9600.c
+++ uart/msp430x54x_uscia0_duplex_9600.c
@@ -1,8 +1,14 @@
 #include <msp430.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-char message[]="hola";
+char command[20];
 
+int i=0;
+int cmdRdy=0;
+char buf[5];
 void sendString(const char *string){
 	int index;
     for(index=0; index < strlen(string); index++){
@@ -11,7 +17,13 @@ void sendString(const char *string){
     }
 }
 
+int char2Int(const char c){
+	int number = c - '0';
+	return number;
+}
+
 int main(void){
+
   WDTCTL = WDTPW+WDTHOLD;                   // Stop watchdog timer
   P7SEL |= 0x03;                            // Port select XT1
 
@@ -33,9 +45,42 @@ int main(void){
   UCA0BR1 = 0x00;                           //
   UCA0MCTL = 0x06;                          // Modulation
   UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-  UCA0IE |= UCTXIE + UCRXIE;                // Enable USCI_A0 TX/RX interrupt
-  __bis_SR_register(LPM3_bits + GIE);       // Enter LPM3 w/ interrupts enabled
-  //__no_operation();                         // For debugger
+  UCA0IE |= UCRXIE;                // Enable USCI_A0 TX/RX interrupt
+  __bis_SR_register(SCG0);       // Enter LPM3 w/ interrupts enabled
+  _enable_interrupt();
+  __no_operation();                         // For debugger
+
+  P1OUT=0x01;
+  while(1){
+	  if (cmdRdy == 1){
+		  cmdRdy=0;
+
+		  if(command[0] == 'D'){
+			  if(command[1] == 'O'){
+				  int port = char2Int(command[2]);
+				  int pin = char2Int(command[3])+1;
+
+				  if (port == 1){
+					  if (command[4] == 'H'){
+						  P1OUT |= pin & (1*(int)(pow(2,pin-1)));
+					  }
+					  else if (command[4] == 'L'){
+						  P1OUT &=  ~(pin & (1*(int)(pow(2,pin-1))));
+					  }
+				  }
+			  }
+		  }
+
+		  i=0;
+
+	  }
+
+  }
+
+
+
+
+
 }
 
 
@@ -44,23 +89,17 @@ __interrupt void USCI_A0_ISR(void){
 
     switch(__even_in_range(UCA0IV,4)){
 		case 0: break;                          // Vector 0 - no interrupt
-		case 2:        						  	// Vector 2 - RXIFG
-			if (UCA0RXBUF == 'a'){
-				P1OUT = 0x01;
-			}
-			else if (UCA0RXBUF == 'b'){
-				P1OUT = 0x02;
+		case 2:
+			if (UCA0RXBUF != '/'){
+				command[i]=UCA0RXBUF;
+				i++;
 			}
 			else{
-				P1OUT = 0x00;
+				cmdRdy=1;
 			}
-
 			break;
-    case 4:                                 // Vector 4 - TXIFG
-      __delay_cycles(5000);                 // Add small gap between TX'ed bytes
-      sendString(message);
-      _delay_cycles(500000);
-      break;
+    case 4:
+    	break;
     default: break;
   }
 }
