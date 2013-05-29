f0e01b50f57612234d26adc511db7798de178a83
Pablo <read.the.disclaimer@gmail.com>
2013-05-30 00:11:59 +0200

    first tests with i2c!!!

---
+++ testing/i2c tx/prueba/main.c
@@ -0,0 +1,78 @@
+#include <msp430.h>
+
+unsigned char *PTxData;                     // Pointer to TX data
+unsigned char TXByteCtr;
+
+const unsigned char TxData[] =              // Table of data to transmit
+{
+
+  0x05,
+  0xF0
+
+};
+
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P3SEL |= 0x06;                            // Assign I2C pins to USCI_B0
+  UCB0CTL1 |= UCSWRST;                      // Enable SW reset
+  UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;     // I2C Master, synchronous mode
+  UCB0CTL1 = UCSSEL_2 + UCSWRST;            // Use SMCLK, keep SW reset
+  UCB0BR0 = 12;                             // fSCL = SMCLK/12 = ~100kHz
+  UCB0BR1 = 0;
+  UCB0I2CSA = 0x60;                         // Slave Address is 048h
+  UCB0CTL1 &= ~UCSWRST;                     // Clear SW reset, resume operation
+  UCB0IE |= UCTXIE;                         // Enable TX interrupt
+
+  while (1)
+  {
+    __delay_cycles(50);                     // Delay required between transaction
+    PTxData = (unsigned char *)TxData;      // TX array start address
+                                            // Place breakpoint here to see each
+                                            // transmit operation.
+    TXByteCtr = sizeof TxData;              // Load TX byte counter
+
+    UCB0CTL1 |= UCTR + UCTXSTT;             // I2C TX, start condition
+
+    __bis_SR_register(LPM0_bits + GIE);     // Enter LPM0, enable interrupts
+    __no_operation();                       // Remain in LPM0 until all data
+                                            // is TX'd
+    while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
+  }
+}
+
+//------------------------------------------------------------------------------
+// The USCIAB0TX_ISR is structured such that it can be used to transmit any
+// number of bytes by pre-loading TXByteCtr with the byte count. Also, TXData
+// points to the next byte to transmit.
+//------------------------------------------------------------------------------
+#pragma vector = USCI_B0_VECTOR
+__interrupt void USCI_B0_ISR(void)
+{
+  switch(__even_in_range(UCB0IV,12))
+  {
+  case  0: break;                           // Vector  0: No interrupts
+  case  2: break;                           // Vector  2: ALIFG
+  case  4:
+	  __no_operation();
+	  break;                           // Vector  4: NACKIFG
+  case  6:
+	  __no_operation();
+	  break;                           // Vector  6: STTIFG
+  case  8: break;                           // Vector  8: STPIFG
+  case 10: break;                           // Vector 10: RXIFG
+  case 12:                                  // Vector 12: TXIFG
+    if (TXByteCtr)                          // Check TX byte counter
+    {
+      UCB0TXBUF = *PTxData++;               // Load TX buffer
+      TXByteCtr--;                          // Decrement TX byte counter
+    }
+    else
+    {
+      UCB0CTL1 |= UCTXSTP;                  // I2C stop condition
+      UCB0IFG &= ~UCTXIFG;                  // Clear USCI_B0 TX int flag
+      __bic_SR_register_on_exit(LPM0_bits); // Exit LPM0
+    }
+  default: break;
+  }
+}
